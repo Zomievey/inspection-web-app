408cc42d6d828e3b18142b3885ba7bbe5d19f3e1
Haylee Hunter <haylee.hunter@sigao.io>
2022-05-25 13:59:24 -0500

    completed inspection web application using angular and .net

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { InspectionComponent } from './inspection/inspection.component';
+import { AddEditInspectionComponent } from './inspection/add-edit-inspection/add-edit-inspection.component';
+import { ShowInspectionComponent } from './inspection/show-inspection/show-inspection.component';
+import { InspectionApiService } from './inspection-api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InspectionComponent,
+    AddEditInspectionComponent,
+    ShowInspectionComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, HttpClientModule, FormsModule, ReactiveFormsModule],
+  providers: [InspectionApiService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
